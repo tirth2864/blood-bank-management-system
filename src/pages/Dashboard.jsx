d9a013805671c85feb8824b44612ec14355ee26e
tirth2864 <tirth2864@gmail.com>
2025-01-29 09:59:08 +0530

    hero and testimonial

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,168 @@
-import React from 'react';
+import React from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import "../styles/dashboard.css";
 
-function Dashboard(){
+function Dashboard() {
   return (
-    <div>
-      This is home page
-    </div>
-  )
-};
+    <section className="dashboard">
+      <section className="dashboard-hero">
+        <h1 className="hero-heading">
+          Welcome,
+          <br />
+          <span className="highlight">
+            You are one step closer to save someone's life!!
+          </span>
+        </h1>
+        <p className="hero-description">
+          Join us in making a difference! Whether you are a donor or a
+          recipient, our platforms connect you to a community dedicated to
+          saving lives!!
+        </p>
+        <div className="hero-buttons">
+          <NavLink to="/donate" className="primary-button">
+            Donate Now
+          </NavLink>
+          <NavLink to="/request" className="secondary-button">
+            Request Blood
+          </NavLink>
+        </div>
+      </section>
+
+      <section className="how-it-works">
+        <h2 className="section-heading">How it Works!</h2>
+        <div className="steps-container">
+          <div className="step">
+            <div className="step-icon">1</div>
+            <h3 className="step-title">Register</h3>
+            <p className="step-description">
+              Sign up as a donor or recipient and create your profile.
+            </p>
+          </div>
+          <div className="step">
+            <div className="step-icon">2</div>
+            <h3 className="step-title">Connect</h3>
+            <p className="step-description">
+              Find donors or recipient in your area through our platform
+            </p>
+          </div>
+          <div className="step">
+            <div className="step-icon">3</div>
+            <h3 className="step-title">Save lives</h3>
+            <p className="step-description">
+              Coordinate donation and help save lives in your community.
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section className="testimonials-section">
+        <div className="container">
+          <div className="section-header">
+            <h2 className="section-title">Testimonials</h2>
+            <p className="section-description">
+              Hear what our donors and recipients have to say about their
+              experience with us!
+            </p>
+          </div>
+          <div className="testimonials-grid">
+            <figure className="testimonial">
+              <blockquote className="testimonial-quote">
+                <h3 className="testimonial-title">
+                  Truly a lifechanging experience.
+                </h3>
+                <p>
+                  "I have never realised how easy it was to donate blood and
+                  save life. The platform connected me with someone in need, and
+                  it was such a rewarding experience!"
+                </p>
+              </blockquote>
+              <figcaption className="testimonial-author">
+                <div className="avatar">
+                  <img
+                    src="https://avatar.iran.liara.run/public/36"
+                    alt="User avatar"
+                    className="avatar-image"
+                  />
+                </div>
+                <div className="author-info">
+                  <span className="author-name">John Doe</span> <br />
+                  <span className="author-role">Blood Donor</span>
+                </div>
+              </figcaption>
+            </figure>
+            <figure className="testimonial">
+              <blockquote className="testimonial-quote">
+                <h3 className="testimonial-title">Saved my life</h3>
+                <p>
+                  "Thanks to this platform, I found a donor when I needed the
+                  most. I am forever grateful for their help and kindness."
+                </p>
+              </blockquote>
+              <figcaption className="testimonial-author">
+                <div className="avatar">
+                  <img
+                    src="https://avatar.iran.liara.run/public/37"
+                    alt="User avatar"
+                    className="avatar-image"
+                  />
+                </div>
+                <div className="author-info">
+                  <span className="author-name">Jane Smith</span> <br />
+                  <span className="author-role">Blood Recipient</span>
+                </div>
+              </figcaption>
+            </figure>
+            <figure className="testimonial">
+              <blockquote className="testimonial-quote">
+                <h3 className="testimonial-title">A Seamless Process</h3>
+                <p>
+                  "The process was so simple and efficient. I highly recommend
+                  anyone who can donate to it. It's and incredible way to help
+                  others!"
+                </p>
+              </blockquote>
+              <figcaption className="testimonial-author">
+                <div className="avatar">
+                  <img
+                    src="https://avatar.iran.liara.run/public/38"
+                    alt="User avatar"
+                    className="avatar-image"
+                  />
+                </div>
+                <div className="author-info">
+                  <span className="author-name">Michael Lee</span> <br />
+                  <span className="author-role">Blood Donor</span>
+                </div>
+              </figcaption>
+            </figure>
+            <figure className="testimonial">
+              <blockquote className="testimonial-quote">
+                <h3 className="testimonial-title">Forever Grateful</h3>
+                <p>
+                  "I cannot thank this platform enough for helping me during an
+                  emergency. The support was immediate, and the donors were
+                  amazing."
+                </p>
+              </blockquote>
+              <figcaption className="testimonial-author">
+                <div className="avatar">
+                  <img
+                    src="https://avatar.iran.liara.run/public/39"
+                    alt="User avatar"
+                    className="avatar-image"
+                  />
+                </div>
+                <div className="author-info">
+                  <span className="author-name">Emily Clark</span> <br />
+                  <span className="author-role">Blood Recipient</span>
+                </div>
+              </figcaption>
+            </figure>
+          </div>
+        </div>
+      </section>
+    </section>
+  );
+}
 
 export default Dashboard;
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -18,35 +18,222 @@ function SignUp() {
     lastDonationDate: "",
     agree: "false",
   });
-}
 
-const navigate = useNavigate();
+  const navigate = useNavigate();
 
-const handleChange = (e) => {
-  const { name, value, type, checked } = e.target;
-  setUser({
-    ...user,
-    [name]: type === "checkbox" ? checked : value,
-  });
-};
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    setUser({
+      ...user,
+      [name]: type === "checkbox" ? checked : value,
+    });
+  };
 
-const handleSubmit = (e) => {
-  e.preventDefault();
-  if (user.password !== user.confirmPassword) {
-    alert("Passwords dont match!!");
-    return;
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (user.password !== user.confirmPassword) {
+      alert("Passwords dont match!!");
+      return;
+    }
 
-  if (!user.agree) {
-    alert("You must agree to the terms and condition");
-    return;
-  }
+    if (!user.agree) {
+      alert("You must agree to the terms and condition");
+      return;
+    }
 
-  alert(
-    `Congrats ${user.fullName}, you are one step closer to save life of someone`
+    alert(
+      `Congrats ${user.fullName}, you are one step closer to save life of someone`
     );
     navigate("/login");
-};
+  };
 
+  return (
+    <div className="signup-container">
+      <h1>Sign-Up</h1>
+      <form onSubmit={handleSubmit} className="signup-form">
+        <div className="form-group">
+          <label htmlFor="fullName">Full Name</label>
+          <input
+            type="text"
+            id="fullName"
+            name="fullName"
+            value={user.fullName}
+            onChange={handleChange}
+            placeholder="Enter your Full Name"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email Address</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={user.email}
+            onChange={handleChange}
+            placeholder="Enter your Email"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={user.password}
+            onChange={handleChange}
+            placeholder="Enter your Password!"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <input
+            type="password"
+            id="confrimPassword"
+            name="confirmPassword"
+            value={user.confirmPassword}
+            onChange={handleChange}
+            placeholder="Confirm your Password"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="mobile">Mobile Number</label>
+          <input
+            type="tel"
+            id="mobile"
+            name="mobile"
+            value={user.mobile}
+            onChange={handleChange}
+            placeholder="Enter your mobile number"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="role">Role</label>
+          <select
+            id="role"
+            name="role"
+            value={user.role}
+            onChange={handleChange}
+            required
+          >
+            <option value="Donor">Donor</option>
+            <option value="Recipinet">Recipinet</option>
+            <option value="Admin">Admin</option>
+          </select>
+        </div>
+        {user.role !== "Admin" && (
+          <div className="form-group">
+            <label htmlFor="blood-group">Blood Group</label>
+            <select
+              name="bloodGroup"
+              id="bloodGroup"
+              value={user.bloodGroup}
+              onChange={handleChange}
+              required
+            >
+              <option value="">Select</option>
+              <option value="A+">A+</option>
+              <option value="A-">A-</option>
+              <option value="B+">B+</option>
+              <option value="B-">B-</option>
+              <option value="AB+">AB+</option>
+              <option value="AB-">AB-</option>
+              <option value="O+">O+</option>
+              <option value="O-">O-</option>
+            </select>
+          </div>
+        )}
 
+        {user.role !== "Admin" && (
+          <div className="form-group">
+            <label htmlFor="dob">Date of Birth</label>
+            <input
+              type="date"
+              id="dob"
+              name="dob"
+              value={user.dob}
+              onChange={handleChange}
+              required
+            />
+          </div>
+        )}
+
+        <div className="form-group">
+          <label htmlFor="gender">Gender</label>
+          <select
+            name="gender"
+            id="gender"
+            value={user.gender}
+            onChange={handleChange}
+            required
+          >
+            <option value="">Select</option>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+            <option value="Other">Other</option>
+          </select>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="address">Address</label>
+          <textarea
+            name="address"
+            id="address"
+            value={user.address}
+            onChange={handleChange}
+            placeholder="Enter your address"
+            required
+          ></textarea>
+        </div>
+
+        {user.role === "Donor" && (
+          <div className="form-group">
+            <label htmlFor="medicalHistory">Medical History</label>
+            <textarea
+              id="medicalHistory"
+              name="medicalHistory"
+              value={user.medicalHistory}
+              onChange={handleChange}
+              placeholder="Mention any known medical conditions (optional)"
+            ></textarea>
+          </div>
+        )}
+
+        {user.role === "Donor" && (
+          <div className="form-group">
+            <label htmlFor="lastDonationDate">Last Donation Date</label>
+            <input
+              type="date"
+              id="lastDonationDate"
+              name="lastDonationDate"
+              value={user.lastDonationDate}
+              onChange={handleChange}
+            />
+          </div>
+        )}
+
+        <div className="form-group">
+          <label>
+            <input
+              type="checkbox"
+              name="agree"
+              checked={user.agree}
+              onChange={handleChange}
+              required
+            />{" "}
+            I agree to the terms and conditions.
+          </label>
+        </div>
+
+        <button type="submit" className="signup-button">
+          Signup
+        </button>
+      </form>
+    </div>
+  );
+}
 export default SignUp;
